4d0e57c09929d89e7f219b3490c1b6ba7c4911ec
Elis KLeen <elisk@net.chalmers.se>
2020-12-10 17:07:27 +0100

    change

--- Zoom.py
+++ Zoom.py
@@ -35,6 +35,7 @@ def waitForZoom():
         err=checkIfSame(reName, promtName)
         if(err<10):
             return
+
             
 def checkIfSame(imgA, imgB):
     hash0 = imagehash.average_hash(Image.open(imgA)) 
